7525bef7e2e8b1b17f6164d0f224d0ee6b6ce192
25730098-PhamNguyenTuanAnh <25730098@ms.uit.edu.vn>
2026-04-18 11:18:43 +0700

    End game when hitting walls

--- main.cpp
+++ main.cpp
@@ -3,25 +3,32 @@
 #include <cstdlib>
 #include <conio.h>
 #include <time.h>
+
 #define MINX 2
 #define MINY 2
 #define MAXX 35
 #define MAXY 20
+
 using namespace std;
+
 void gotoxy(int column, int line);
+
 struct Point {
 	int x, y;
 };
+
 class CONRAN {
 public:
 	struct Point A[100];
 	int DoDai;
+
 	CONRAN() {
 		DoDai = 3;
-		A[0].x = 10; A[0].y = 10;
+		A[0].x = 12; A[0].y = 10;
 		A[1].x = 11; A[1].y = 10;
-		A[2].x = 12; A[2].y = 10;
+		A[2].x = 10; A[2].y = 10;
 	}
+
 	void Ve(Point Qua) {
 		for (int i = 0; i < DoDai; i++) {
 			gotoxy(A[i].x, A[i].y);
@@ -30,6 +37,7 @@ public:
 		gotoxy(Qua.x, Qua.y);
 		cout << "*";
 	}
+
 	void DiChuyen(int Huong, Point& Qua) {
 		for (int i = DoDai - 1; i > 0; i--)
 			A[i] = A[i - 1];
@@ -39,11 +47,12 @@ public:
 		if (Huong == 3) A[0].y = A[0].y - 1;
 		if ((A[0].x == Qua.x) && (A[0].y == Qua.y)) {
 			DoDai++;
-			Qua.x = rand() % (MAXX - MINX) + MINX;
-			Qua.y = rand() % (MAXY - MINY) + MINY;
+			Qua.x = rand() % (MAXX - MINX - 1) + MINX + 1;
+			Qua.y = rand() % (MAXY - MINY - 1) + MINY + 1;
 		}
 	}
 };
+
 void VeKhung()
 {
 	for (int i = MINX; i <= MAXX; i++)
@@ -53,11 +62,12 @@ void VeKhung()
 			if (i == MINX || i == MAXX || j == MINY || j == MAXY)
 			{
 				gotoxy(i, j);
-				cout << "*";
+				cout << "+";
 			}
 		}
 	}
 }
+
 int main()
 {
 	CONRAN r;
@@ -65,8 +75,8 @@ int main()
 	int t;
 	Point Qua;
 	srand((int)time(0));
-	Qua.x = rand() % (MAXX - MINX) + MINX;
-	Qua.y = rand() % (MAXY - MINY) + MINY;
+	Qua.x = rand() % (MAXX - MINX - 1) + MINX + 1;
+	Qua.y = rand() % (MAXY - MINY - 1) + MINY + 1;
 	while (1) {
 		if (_kbhit()) {
 			t = _getch();
@@ -88,12 +98,29 @@ int main()
 		VeKhung();
 		r.Ve(Qua);
 		r.DiChuyen(Huong, Qua);
+
+		bool hitBorder = r.A[0].x <= MINX || r.A[0].x >= MAXX
+			|| r.A[0].y <= MINY || r.A[0].y >= MAXY;
+		bool hitSelf = false;
+		for (int i = 1; i < r.DoDai; i++) {
+			if (r.A[0].x == r.A[i].x && r.A[0].y == r.A[i].y) {
+				hitSelf = true;
+				break;
+			}
+		}
+		if (hitBorder || hitSelf) {
+			gotoxy(MINX, MAXY + 2);
+			cout << "Game Over! " << (hitBorder ? "Hit the wall." : "Hit yourself.")
+				<< " Score: " << (r.DoDai - 3) << ". Press any key to exit.";
+			_getch();
+			break;
+		}
+
 		Sleep(300);
 	}
 
 	return 0;
 }
-
 
 void gotoxy(int column, int line)
 {
